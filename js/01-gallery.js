135ef97546af42269b30449f093fb7a96bb3de53
Olyazarichna <olya.zarichna@gmail.com>
2022-05-01 11:19:42 +0300

    task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,20 +1,65 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
 
+const galleryContainer = document.querySelector('.gallery');
 
-{/* <div class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
+// розмітка для галереї
+function markupGallery() {
+  const markup = galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
-      src="small-image.jpg"
-      data-source="large-image.jpg"
-      alt="Image description"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
     />
   </a>
-</div> */}
+</div>
+        `;
+    })
+    .join('');
+  galleryContainer.insertAdjacentHTML('beforeend', markup);
+}
 
-const galleryEl = document.querySelector('.gallery');
+markupGallery();
 
+galleryContainer.addEventListener('click', onGalleryContainerClick);
 
+// callback ф-ція для click
+function onGalleryContainerClick(event) {
+  event.preventDefault();
+  console.log(event.target.nodeName);
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  } else {
+    //з бібліотеки
+    const instance = basicLightbox.create(`
+        <img src="${event.target.dataset.source}" width="800" height="600">
+         `);
+    instance.show();
+    // console.log(event.target.dataset.source);
+    // document.addEventListener('keydown', onModalKeydown);
+  }
+}
+
+//Додай закриття модального вікна після натискання клавіші Escape. Зроби так, щоб прослуховування клавіатури було тільки доти, доки відкрите модальне вікно. Бібліотекаи basicLightbox містить метод для програмного закриття модального вікна.
+
+// document.addEventListener('keydown', onModalKeydown);
+
+// function onModalKeydown(event) {
+
+//     if(event.code !== "Escape") {
+//         return;
+//     } else {
+
+//          console.log("code: ", event.code);
+// const instance = basicLightbox.create(`
+       
+//      instance.close();
+//     }
+
+// };
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,40 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const listGallery = document.querySelector('.gallery');
+
+console.log(listGallery);
+
+function markupListGallery() {
+  const markup = galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <a class="gallery__item" href="${origin}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a> `;
+    })
+    .join('');
+  listGallery.insertAdjacentHTML('beforeend', markup);
+}
+markupListGallery();
+
+
+listGallery.addEventListener('click', onGalleryClick);
+
+function onGalleryClick(event) {
+    event.preventDefault();
+    console.log(event.target.nodeName);
+    if(event.target.nobename !== 'IMG') {
+        return;
+    } else {
+        let gallery = new SimpleLightbox('.gallery a');
+        gallery.on('show.simplelightbox', function () {
+           console.log("njk")
+        });
+        
+        gallery.on('error.simplelightbox', function (e) {
+            console.log(e); // Some usefull information
+        });
+  
+    }};
